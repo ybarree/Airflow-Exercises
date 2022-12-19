7152ec6cd1c55fe05de56863417a29902f752885
Yasmin Barree <88638956+ybarree@users.noreply.github.com>
2022-12-19 20:34:39 +0300

    Update Exercise3.py

--- Exercise3.py
+++ Exercise3.py
@@ -9,34 +9,33 @@ from datetime import datetime, timedelta
 
 from airflow.operators.python_operator import PythonOperator
 
-# Define the API key outside of the function so it can be used in multiple requests
+# Defining the API key 
 api_key = 'GLB0E7DZNVDFGHD3'
 
-# Define the path to the DATA_LAKE folder
+# Defining the path to the folder
 path = "/Users/yasminbarree/DATA_CENTER/DATA_LAKE"
 
 def get_data(**kwargs):
-    # Define the URLs with the correct API key
+    # Defining the URLs with the correct API key
     url1 = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=' + api_key
     url2 = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSCO.LON&outputsize=full&apikey=' + api_key
     url3 = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=SHOP.TRT&outputsize=full&apikey=' + api_key
 
-    # Create a list of URLs
+    # Creating a list of URLs
     urls = [url1, url2, url3]
 
-    # Iterate through the list of URLs
     for url in urls:
         # Make request to the URL
         r = requests.get(url)
         try:  
             data = r.json()
-            # Save the data to a file in the DATA_LAKE folder
+            # Saving the data to a file in the DATA_LAKE folder
             with open(os.path.join(path, "market_data.json"), "w") as f:
                 json.dump(data, f)
         except:
-            print("Error with API request")
+            print("Error")
 
-# Set the default arguments for the DAG
+# Setting the default arguments for the DAG
 default_dag_args = {
     'start_date': datetime(2022, 12, 15),
     'email_on_failure': False,
@@ -46,7 +45,7 @@ default_dag_args = {
     'project_id': 1
 }
 
-# Create the DAG using the default arguments and a schedule interval of daily
-with DAG("market_data_dag_2", schedule_interval = '@daily', catchup=False, default_args = default_dag_args) as dag_python:
-    # Create a PythonOperator task to run the get_data function
+# Creating the DAG using the default arguments and a schedule interval of daily
+with DAG("themarket_data", schedule_interval = '@daily', catchup=False, default_args = default_dag_args) as dag_python:
+    # Creating a PythonOperator task to run the get_data function
     task_0 = PythonOperator(task_id = "get_market_data", python_callable = get_data)
