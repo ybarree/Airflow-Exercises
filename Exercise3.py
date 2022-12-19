44e75daacc9f03f3bd9a541d0bfa2ae4aa647fa2
Yasmin Barree <88638956+ybarree@users.noreply.github.com>
2022-12-19 20:33:18 +0300

    Update Exercise3.py

--- Exercise3.py
+++ Exercise3.py
@@ -1,4 +1,3 @@
-
 import requests
 import time
 import json
@@ -6,71 +5,48 @@ import pandas as pd
 from airflow import DAG
 import numpy as np
 import os
-
-from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 
-#to make your function accept parameters , you must define key arguments -> kwargs
-#by writing kwargs your airflow task is expecting dictionary or arguments 
+from airflow.operators.python_operator import PythonOperator
+
+# Define the API key outside of the function so it can be used in multiple requests
+api_key = 'GLB0E7DZNVDFGHD3'
+
+# Define the path to the DATA_LAKE folder
+path = "/Users/yasminbarree/DATA_CENTER/DATA_LAKE"
 
 def get_data(**kwargs):
-    api_key = 'GLB0E7DZNVDFGHD3'
+    # Define the URLs with the correct API key
+    url1 = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=' + api_key
+    url2 = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSCO.LON&outputsize=full&apikey=' + api_key
+    url3 = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=SHOP.TRT&outputsize=full&apikey=' + api_key
 
-    # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo' + api_key
-    r = requests.get(url)
+    # Create a list of URLs
+    urls = [url1, url2, url3]
 
-    try:  
-        data = r.json()
-        path = "/Users/yasminbarree/DATA_CENTER/DATA_LAKE/"
-        with open(path + "stock _market_raw_data" + "IBM_" + str(time. time()), "W") as outfile:
-            json.dump(data, outfile)
-    except:
-        pass
+    # Iterate through the list of URLs
+    for url in urls:
+        # Make request to the URL
+        r = requests.get(url)
+        try:  
+            data = r.json()
+            # Save the data to a file in the DATA_LAKE folder
+            with open(os.path.join(path, "market_data.json"), "w") as f:
+                json.dump(data, f)
+        except:
+            print("Error with API request")
 
-def test_data(**kwargs):
-    pass
-
-
-def clean_data(**kwargs):
-  read_path = "/Users/yasminbarree/DATA_CENTER/DATA_LAKE/"
-    #get a way to read the most file from dictory of data 
-  ticker = kwargs['ticker']
-  lastest = np.max([float(file.split('_')[-1]) for file in os.listdir(read_path) if ticker in file])
-  latest_file = [file for file in os.listdir(read_path) if str(latest) in file ][0]
-
-
-  output_path = "/Users/yasminbarree/DATA_CENTER/CLEAN_DATA/"
-
-  #loading the raw data from the file 
-  file = open(read_path + latest_file)
-  data = json.load(file)
-
-
-  
-  clean_data = pd.DataFrame(data['Time Series (Daily)']).T 
-  clean_data['ticker'] = data['Meta Data']['2. Symbol']
-  clean_data['Meta Data'] = str(data['Meta Data'])
-  clean_data['timestamp'] = pd.to_datetime('now')
-
-  # storing the data in the clean data lake 
-  clean_data.to_csv(output_path + 'IBM_snapshot_daily' + str(pd.to_datetime('now')) + '.csv')
-  
-
-
+# Set the default arguments for the DAG
 default_dag_args = {
-'start_date': datetime (2022, 12, 15),
-'email_on_failure': False,
-'email_on_retry': False,
-'retries': 1,
-'retry_delay': timedelta (minutes=5),
-'project_id': 1
-
+    'start_date': datetime(2022, 12, 15),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    'project_id': 1
 }
 
-with DAG("market_data_dag", schedule_interval = '@daily', catchup=False, default_args = default_dag_args) as dag_python:
-
-
-
-    task_0 = PythonOperator(task_id = "get_market_data", python_callable = get_data,op_kwargs = {'ticker' :'IBM'})
-task_0
+# Create the DAG using the default arguments and a schedule interval of daily
+with DAG("market_data_dag_2", schedule_interval = '@daily', catchup=False, default_args = default_dag_args) as dag_python:
+    # Create a PythonOperator task to run the get_data function
+    task_0 = PythonOperator(task_id = "get_market_data", python_callable = get_data)
